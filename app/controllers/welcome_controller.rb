391775ca4440c1707fb523ec3ca2e65cb0a251b4
Diogo Scudelletti <diogo@scudelletti.com>
2015-08-16 11:17:56 -0300

    Add Health Check and Send Mail

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,17 @@
 class WelcomeController < ApplicationController
   def index
-    render text: "HOSTNAME: #{ENV['HOSTNAME']}"
+    SomethingMailer.welcome_mail(hostname).deliver_now
+
+    render text: "RAILS APP - HOSTNAME: #{hostname}"
+  end
+
+  def health_check
+    head 200
+  end
+
+  private
+
+  def hostname
+    ENV['HOSTNAME']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get 'health-check' => 'welcome#health_check'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
